dfbc9bafbbcdfd947d4813dcd327eaefa1bd0dcd
Kiboom <cheyugy@naver.com>
2015-07-15 22:06:01 +0900

    Update Car_Test.java

--- 141009 김기범 - 자동차 과제/Car_Test.java
+++ 141009 김기범 - 자동차 과제/Car_Test.java
@@ -3,9 +3,9 @@ package car;
 public class Car_Test {
 
 	public static void main(String[] args) {
-		CarCompany KiboomCorp = CarCompany.getInstance();		// Single Tone으로 자동차 회사 선언
+		CarCompany KiboomCorp = CarCompany.getInstance();	// Single Tone으로 자동차 회사 선언
 		
-		for(int i=0 ; i<6 ; i++){					// 자동차 회사에 차종별로 입고!
+		for(int i=0 ; i<6 ; i++){				// 자동차 회사에 차종별로 입고!
 			if(i<3){
 				KiboomCorp.addCar(new Sonata());
 			}
@@ -19,5 +19,4 @@ public class Car_Test {
 			
 		KiboomCorp.print_SumPrices();				// 보유한 자동차의 총 가격 출력!
 	}
-
 }
