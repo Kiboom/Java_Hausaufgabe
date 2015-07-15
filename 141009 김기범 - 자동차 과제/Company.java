194d770f9d47f3de1c0c847badd67ceba0aa92e5
Kiboom <cheyugy@naver.com>
2015-07-15 23:08:29 +0900

    Update Company.java

--- 141009 김기범 - 자동차 과제/Company.java
+++ 141009 김기범 - 자동차 과제/Company.java
@@ -3,22 +3,22 @@ package car;
 import java.util.ArrayList;
 
 public class Company {
-	private ArrayList<Car> CarList = new ArrayList<Car>();	// 보유한 자동차 리스트
-	private static Company company = new Company();	// Single Tone 만들기
+	private ArrayList<Car> CarList = new ArrayList<Car>();		// 보유한 자동차 리스트
+	private static Company company = new Company();			// Single Tone 만들기
 	
-	private Company(){}									// Single Tone 만들기
+	private Company(){}						// Single Tone 만들기
 	
 	public ArrayList<Car> getList(){
 		return CarList;
 	}
 	
-	public static Company getInstance(){					// Single Tone 만들기
+	public static Company getInstance(){				// Single Tone 만들기
 		if(company == null)
 			company = new Company();
 		return company;
 	}
 
-	public void addNewCar(Car car){							// 자동차 입고!
+	public void addNewCar(Car car){					// 자동차 입고!
 		CarList.add(car);
 	}	
 }
