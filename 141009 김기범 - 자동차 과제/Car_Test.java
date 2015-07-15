7e9a9d5942942f9ae0913d26385ce751e7d6e0fa
Kiboom <cheyugy@naver.com>
2015-07-15 23:06:46 +0900

    change

---
+++ 141009 김기범 - 자동차 과제/Car_Test.java
@@ -0,0 +1,20 @@
+package car;
+
+public class Car_Test {
+
+	public static void main(String[] args) {
+		Company KiboomCorp = Company.getInstance();		// Single Tone으로 자동차 회사 선언
+		Calculator calc = new Calculator();
+		
+		KiboomCorp.addNewCar(new Sonata());
+		KiboomCorp.addNewCar(new Sonata());
+		KiboomCorp.addNewCar(new Sonata());	
+		KiboomCorp.addNewCar(new Grandeur());
+		KiboomCorp.addNewCar(new Grandeur());	
+		KiboomCorp.addNewCar(new Genesis());
+			
+		int Sum = calc.SumPrices(KiboomCorp);				
+		System.out.println("귀사가 보유한 자동차 총 가격 : " + Sum +"$");
+	}
+
+}
