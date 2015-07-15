14fe40fd5fae630c117a4d3f8dd60d000d76f3db
Kiboom <cheyugy@naver.com>
2015-07-15 21:46:41 +0900

    CarCompany 수정

--- 141009 김기범 - 자동차 과제/Car_Test.java
+++ 141009 김기범 - 자동차 과제/Car_Test.java
@@ -1,29 +1,26 @@
 package car;
 
-import java.util.ArrayList;
-
 public class Car_Test {
 
 	public static void main(String[] args) {
-		Car_Company KiboomCorp = Car_Company.getInstance();		// Single Tone으로 자동차 회사 선언
-		ArrayList<Car> CarList = new ArrayList<Car>();		// 보유한 자동차의 배열
+		CarCompany KiboomCorp = CarCompany.getInstance();		// Single Tone으로 자동차 회사 선언
 		
-		for (int i=0 ; i<6 ; i++){								// 배열에 Car 객체 삽입
-			if(i<3){	
-				Sonata a = new Sonata();
-				CarList.add(a);
+		for(int i=0 ; i<6 ; i++){					// 자동차 회사에 차종별로 입고!
+			if(i<3){
+				Sonata sonata = new Sonata();
+				KiboomCorp.addCar(sonata);
 			}
 			else if(i<5){
-				Grandeur b = new Grandeur();
-				CarList.add(b);
+				Grandeur grandeur = new Grandeur();
+				KiboomCorp.addCar(grandeur);
 			}
 			else{
-				Genesis c = new Genesis();
-				CarList.add(c);
+				Genesis genesis = new Genesis();
+				KiboomCorp.addCar(genesis);
 			}
 		}
-		
-		KiboomCorp.print_Sum_CarPrices(CarList);				// 보유한 자동차의 총 가격 출
+			
+		KiboomCorp.print_SumPrices();				// 보유한 자동차의 총 가격 출력!
 	}
 
 }
