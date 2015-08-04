9b399862f46f9d40b1f8391e68f23c4d9848289a
Kiboom <cheyugy@naver.com>
2015-08-05 02:33:03 +0900

    change

--- 141009 김기범 - 성적 계산기 과제/Grade.java
+++ 141009 김기범 - 성적 계산기 과제/Grade.java
@@ -5,14 +5,15 @@ public class Grade {
 	
 	/* 학생의 점수를 학점화 */
 	public void scoreToGrade(Student student){
+		GradingStrategy gs;
 		
 		for(Subject s : student.enrollList){
 			if(s.getName().equals(student.getMajor())){		// 해당 과목이 전공인 경우
-				MajorGrading mg = new MajorGrading();
-				s.setGrade(mg.generateGrade(s.getScore()));	// 전공생 평가 기준을 적용
+				gs = new MajorGrading();
+				s.setGrade(gs.generateGrade(s.getScore()));	// 전공생 평가 기준을 적용
 			}
-			BasicGrading bg = new BasicGrading();
-			s.setGrade(bg.generateGrade(s.getScore()));		// 비전공생 평가 기준을 적용
+			gs = new BasicGrading();
+			s.setGrade(gs.generateGrade(s.getScore()));		// 비전공생 평가 기준을 적용
 		}
 	}
 	
